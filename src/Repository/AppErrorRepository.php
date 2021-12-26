e764426d5a2b4481d249b7e7622cbaa5493db580
Antonio Jiménez <antoniojr997@gmail.com>
2021-12-26 19:53:20 +0100

    (#5) Repositorios. Se crea repositorio base para los métodos de propiedades más comunes. Se mueve método findByType a AppRepository.

--- src/Repository/AppErrorRepository.php
+++ src/Repository/AppErrorRepository.php
@@ -3,10 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\AppError;
-use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\Interfaces\AppErrorRepositoryInterface;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @method AppError|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,13 +12,13 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
  * @method AppError[]    findAll()
  * @method AppError[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class AppErrorRepository extends ServiceEntityRepository implements AppErrorRepositoryInterface
+class AppErrorRepository extends AppRepository implements AppErrorRepositoryInterface
 {
 
     /************************************************* CONSTRUCT **************************************************/
 
     /**
-     * AppError constructor.
+     * AppErrorRepository constructor.
      *
      * @param ManagerRegistry $registry Object managers for a Doctrine persistence layer ManagerRegistry class.
      */
@@ -30,22 +28,6 @@ class AppErrorRepository extends ServiceEntityRepository implements AppErrorRepo
     }
 
     /*********************************************** PUBLIC METHODS ***********************************************/
-
-    /**
-     * @inheritDoc
-     * @return array array
-     */
-    public function findByType(int $type): array
-    {
-        $alias = 'err';
-
-        return $this->createQueryBuilder($alias)
-            ->andWhere($alias . '.type = :type')
-            ->setParameter('type', $type)
-            ->orderBy($alias . '.id', 'ASC')
-            ->getQuery()
-            ->getArrayResult();
-    }
 
     /*********************************************** STATIC METHODS ***********************************************/
 
--- src/Repository/Interfaces/AppRepositoryInterface.php
+++ src/Repository/Interfaces/AppRepositoryInterface.php
@@ -3,10 +3,20 @@
 namespace App\Repository\Interfaces;
 
 
-interface AppErrorRepositoryInterface
+interface AppRepositoryInterface
 {
 
     /*********************************************** PUBLIC METHODS ***********************************************/
 
+    /**
+     * Finds the errors with the specified type.
+     *
+     * @param int $type Type of the error.
+     *
+     * @return array array
+     */
+    public function findByType(int $type): array;
+
     /*********************************************** STATIC METHODS ***********************************************/
+
 }
