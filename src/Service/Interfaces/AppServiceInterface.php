9efd3703dec49aa1fe2b5443c85209381d738ae7
Antonio Jiménez <antoniojr997@gmail.com>
2021-12-26 19:42:05 +0100

    :construction:

--- src/Service/Interfaces/AppServiceInterface.php
+++ src/Service/Interfaces/AppServiceInterface.php
@@ -3,6 +3,9 @@
 namespace App\Service\Interfaces;
 
 
+use App\Entity\AbstractORM;
+use App\Entity\AppError;
+use App\Entity\Interfaces\AbstractORMInterface;
 use Doctrine\Persistence\ObjectManager;
 
 interface AppServiceInterface
@@ -45,6 +48,46 @@ interface AppServiceInterface
     public function setEntityManager(ObjectManager $entityManager): self;
 
     /*********************************************** PUBLIC METHODS ***********************************************/
+
+    /**
+     * Persists an ORM object.
+     *
+     * @param AbstractORM $object Object to persist in the app.
+     *
+     * @return bool bool
+     */
+    public function persistAndFlush(AbstractORMInterface $object): bool;
+
+    /**
+     * Logs an error when trying to persist an ORM object.
+     *
+     * @param string $method Method where the error occurred.
+     * @param int $exceptionCode Code of the exception catch.
+     * @param string $exceptionMessage Message of the exception catch.
+     * @param array $exceptionTrace Trace of the exception catch.
+     *
+     * @return AppError AppError
+     */
+    public function registerPersistException(string $method, int $exceptionCode, string $exceptionMessage,
+                                             array  $exceptionTrace): AppError;
+
+    /**
+     * Logs an error.
+     *
+     * @param string $method Method where the error occurred.
+     * @param int $type Type of the AppError.
+     * @param string $message Message of the AppError.
+     * @param int|null $exceptionCode Code of the exception catch.
+     * @param string|null $exceptionMessage Message of the exception catch.
+     * @param array $exceptionTrace Trace of the exception catch.
+     * @param bool $notify Boolean to notify or not the error.
+     * @param bool $persist Boolean to persist or not the error.
+     *
+     * @return AppError AppError
+     */
+    public function registerAppError(string  $method, int $type, string $message, ?int $exceptionCode = NULL,
+                                     ?string $exceptionMessage = NULL, array $exceptionTrace = array(),
+                                     bool    $notify = TRUE, bool $persist = TRUE): AppError;
 
     /*********************************************** STATIC METHODS ***********************************************/
 
