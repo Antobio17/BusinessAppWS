d3572c8f9d47b792beffffe7825945106d4286af
Antonio Jiménez <antoniojr997@gmail.com>
2022-01-27 20:58:36 +0100

    (#18) UserController. Se crea controlador e interfaz para usuario. Se añade función de acceso para registro de usuario. Se añade ruta a security.yml para poder acceder como anónimo.

---
+++ src/Controller/Interfaces/UserControllerInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Interfaces;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Service\Traits\Interfaces\HasUserServiceInterface;
+
+interface UserControllerInterface extends HasUserServiceInterface
+{
+
+    /************************************************** ROUTING ***************************************************/
+
+    /**
+     * Route to access user registration in the application.
+     *
+     * @param Request $request Request of the route.
+     *
+     * @return JsonResponse JsonResponse
+     */
+    public function signup(Request $request): Response;
+
+    /******************************************** GETTERS AND SETTERS *********************************************/
+
+    /*********************************************** PUBLIC METHODS ***********************************************/
+
+    /*********************************************** STATIC METHODS ***********************************************/
+
+}
