b7ab96fd05c851d4862329dc9bdc15af8e428272
Antonio Jiménez <antoniojr997@gmail.com>
2022-01-29 12:22:24 +0100

    (#18) Codificación de contraseña. Se añade UserPasswordHasher a UserService para el encriptado de contraseñas de usuario. Modificación de código de respuesta y mensaje cuando el usuario ya existe.

--- src/Service/Interfaces/UserServiceInterface.php
+++ src/Service/Interfaces/UserServiceInterface.php
@@ -3,10 +3,29 @@
 namespace App\Service\Interfaces;
 
 
+use App\Service\UserService;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 interface UserServiceInterface extends AppServiceInterface
 {
 
     /******************************************** GETTERS AND SETTERS *********************************************/
+
+    /**
+     * Gets the UserPasswordHasher to encode the user password.
+     *
+     * @return UserPasswordHasherInterface UserPasswordHasherInterface
+     */
+    public function getUserPasswordHasher(): UserPasswordHasherInterface;
+
+    /**
+     * Sets the UserPasswordHasher to encode the user password.
+     *
+     * @param UserPasswordHasherInterface $userPasswordHasher Hasher to encode the user password.
+     *
+     * @return $this $this
+     */
+    public function setUserPasswordHasher(UserPasswordHasherInterface $userPasswordHasher): self;
 
     /*********************************************** PUBLIC METHODS ***********************************************/
 
