873fad5889262a4407d4449606193533cb8d3c9d
Antonio Jiménez <antoniojr997@gmail.com>
2022-01-29 12:39:34 +0100

    :writing_hand:

--- src/Service/Interfaces/UserServiceInterface.php
+++ src/Service/Interfaces/UserServiceInterface.php
@@ -28,6 +28,18 @@ interface UserServiceInterface extends AppServiceInterface
     public function setUserPasswordHasher(UserPasswordHasherInterface $userPasswordHasher): self;
 
     /*********************************************** PUBLIC METHODS ***********************************************/
+    /**
+     * Registers a new user in the application.
+     *
+     * @param string $email The email of the new user.
+     * @param string $password The password of the new user.
+     * @param string $phoneNumber The phone number of the new user.
+     * @param string $name The name of the new user.
+     * @param string $surname The surname of the new user.
+     *
+     * @return bool bool
+     */
+    public function signup(string $email, string $password, string $phoneNumber, string $name, string $surname): bool;
 
     /*********************************************** STATIC METHODS ***********************************************/
 
