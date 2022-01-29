1112766b1005b2e830d81593ba54df70c2c65f4c
Antonio Jiménez <antoniojr997@gmail.com>
2022-01-29 12:22:24 +0100

    (#18) Codificación de contraseña. Se añade UserPasswordHasher a UserService para el encriptado de contraseñas de usuario. Modificación de código de respuesta y mensaje cuando el usuario ya existe.

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -7,6 +7,8 @@ use App\Entity\User;
 use App\Helper\ToolsHelper;
 use App\Service\Interfaces\UserServiceInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserService extends AppService implements UserServiceInterface
 {
@@ -14,6 +16,11 @@ class UserService extends AppService implements UserServiceInterface
     /************************************************* CONSTANTS **************************************************/
 
     /************************************************* PROPERTIES *************************************************/
+
+    /**
+     * @var UserPasswordHasherInterface
+     */
+    protected UserPasswordHasherInterface $userPasswordHasher;
 
     /************************************************* CONSTRUCT **************************************************/
 
@@ -23,13 +30,38 @@ class UserService extends AppService implements UserServiceInterface
      * @param ManagerRegistry $doctrine Doctrine to manage the ORM.
      * @param TelegramService $telegramService Service of Telegram.
      * @param bool $testMode Boolean to set the Test Mode.
+     * @param UserPasswordHasherInterface $userPasswordHasher Hasher to encode the user password.
+     *
      */
-    public function __construct(ManagerRegistry $doctrine, TelegramService $telegramService, bool $testMode = FALSE)
+    public function __construct(ManagerRegistry $doctrine, TelegramService $telegramService,
+        UserPasswordHasherInterface $userPasswordHasher, bool $testMode = FALSE)
     {
         parent::__construct($doctrine, $telegramService, $testMode);
+
+        $this->setUserPasswordHasher($userPasswordHasher);
     }
 
     /******************************************** GETTERS AND SETTERS *********************************************/
+
+    /**
+     * @inheritDoc
+     * @return UserPasswordHasherInterface UserPasswordHasherInterface
+     */
+    public function getUserPasswordHasher(): UserPasswordHasherInterface
+    {
+        return $this->userPasswordHasher;
+    }
+
+    /**
+     * @inheritDoc
+     * @return $this $this
+     */
+    public function setUserPasswordHasher(UserPasswordHasherInterface $userPasswordHasher): self
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+
+        return $this;
+    }
 
     /*********************************************** PUBLIC METHODS ***********************************************/
 
@@ -52,12 +84,14 @@ class UserService extends AppService implements UserServiceInterface
 
         if ($user === NULL):
             $user = new User($email, $password, $phoneNumber, $name, $surname);
+            $encodedPassword = $this->getUserPasswordHasher()->hashPassword($user, $password);
+            $user->setPassword($encodedPassword);
             $this->persistAndFlush($user);
         else:
             $this->registerAppError(
                     ToolsHelper::getStringifyMethod(get_class($this), __FUNCTION__),
-                    AppError::ERROR_BUSINESS_CONTEXT,
-                    'El introducido email ya existe.',
+                    Response::HTTP_CONFLICT,
+                    'El email introducido ya existe.',
                     NULL, NULL, array(),
                     FALSE, FALSE
             );
