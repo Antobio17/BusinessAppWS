8aca6060bf62172fc23ccac86c00ca6fa884764e
Antonio Jiménez <antoniojr997@gmail.com>
2022-02-20 17:09:38 +0100

    :construction:

---
+++ tests/Appointments/BookAppointmentTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Appointments;
+
+use PHPUnit\Framework\TestCase;
+
+class BookAppointmentTest extends TestCase
+{
+    public function test_bookSuccess()
+    {
+        $this->assertFalse(FALSE);
+    }
+}
