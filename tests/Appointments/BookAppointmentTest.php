61244312cd620eebaa96b7fd3bdb74c13d703fa1
Antonio Jiménez <antoniojr997@gmail.com>
2022-03-13 17:29:23 +0100

    Testing BBDD para Github Action...

--- tests/Appointments/BookAppointmentTest.php
+++ tests/Appointments/BookAppointmentTest.php
@@ -2,12 +2,25 @@
 
 namespace App\Tests\Appointments;
 
+use App\Entity\AppError;
+use App\Service\AppService;
+use App\Service\Traits\RepositoriesTrait;
+use Doctrine\Persistence\ObjectManager;
 use PHPUnit\Framework\TestCase;
 
 class BookAppointmentTest extends TestCase
 {
-    public function test_bookSuccess()
+
+    use RepositoriesTrait;
+
+    public function test_bookSuccess(ObjectManager $entityManager)
     {
-        $this->assertFalse(FALSE);
+        $appError = new AppError(1, 'probando', 'probando');
+        $entityManager->persist($appError);
+        $entityManager->flush();
+
+        $newError = $this->getAppErrorRepository()->findAll();
+
+        $this->assertFalse(count($newError) === 1);
     }
 }
