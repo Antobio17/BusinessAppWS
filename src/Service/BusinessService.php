a63a77fb927e6a1f71472ac05f6fa1717f97e76d
Antonio Jiménez <antoniojr997@gmail.com>
2022-07-24 17:51:30 +0200

    (#66) Endpoint para la recuperación de configuración del Home. Se ha creado ruta para obtener la configuración de la página Home del negocio. Se ha creado funcionalidad en BusinessService para obtener la configuración de la página Home del negocio en formato array.

--- src/Service/BusinessService.php
+++ src/Service/BusinessService.php
@@ -2,6 +2,8 @@
 
 namespace App\Service;
 
+use App\Entity\AppError;
+use App\Helper\ToolsHelper;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Service\Interfaces\BusinessServiceInterface;
 
@@ -29,6 +31,55 @@ class BusinessService extends AppService implements BusinessServiceInterface
     /******************************************** GETTERS AND SETTERS *********************************************/
 
     /*********************************************** PUBLIC METHODS ***********************************************/
+
+    /**
+     * @inheritDoc
+     * @return array|null array|null
+     */
+    public function getBusinessHomeConfig(): ?array
+    {
+        $method = ToolsHelper::getStringifyMethod(get_class($this), __FUNCTION__);
+        $business = $this->getBusiness();
+
+        if ($business === NULL):
+            $this->registerAppError_BusinessContextUndefined($method);
+        else:
+            $homeConfig = $this->getHomeConfigRepository()->findByBusiness($business);
+            if ($homeConfig === NULL):
+                $this->registerAppError(
+                    $method, AppError::ERROR_BUSINESS_HOME_CONFIG_UNDEFINED,
+                    'Error al recuperar configuración de Home: no existe configuración',
+                );
+            else:
+                $postalAddress = $business->getPostalAddress();
+                if ($postalAddress !== NULL):
+                    $address = sprintf('%s', $postalAddress->getAddress());
+                endif;
+
+                $config = array(
+                    'intro' => array(
+                        'imageSrc' => $homeConfig->getSRC(),
+                        'bossName' => $homeConfig->getName(),
+                        'description' => $homeConfig->getDescription()
+                    ),
+                    'social' => $homeConfig->getSocialData(),
+                    'services' => $homeConfig->getServicesData(),
+                    'contact' => array(
+                        'businessName' => $business->getName(),
+                        'address' => $address ?? NULL,
+                        'businessHours' => $business->getHours(),
+                        'phoneNumber' => $business->getPhoneNumber(),
+                    )
+                );
+
+                if ($business->getEmail() !== NULL):
+                    $config['contact']['email'] = $business->getEmail();
+                endif;
+            endif;
+        endif;
+
+        return $config ?? NULL;
+    }
 
     /********************************************** PROTECTED METHODS *********************************************/
 
