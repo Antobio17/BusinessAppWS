d91c2184360f852701d0c1545a442626542ee865
Antonio Jiménez <antoniojr997@gmail.com>
2022-07-27 16:55:21 +0200

    (#72) Instalación de Bundles. Se ha instalado EasyAdminBundle y se han creado los archivos base. Se ha instalado WebpackEncoreBundle y se han creado los archivos base. Se crea el archivo webpack.config.js para la compilación de los assets de la administración.

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
