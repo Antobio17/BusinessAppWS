9ef775dbdff629c42d0ebfd07fed06ddb93618da
Antonio Jiménez <antoniojr997@gmail.com>
2022-07-27 20:47:10 +0200

    :hammer:

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,19 +3,40 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use App\Controller\Admin\Interfaces\UserCrudControllerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
-class UserCrudController extends AbstractCrudController
+class UserCrudController extends AbstractCrudController implements UserCrudControllerInterface
 {
+
+    /************************************************* CONSTANTS **************************************************/
+
+    /************************************************* PROPERTIES *************************************************/
+
+    /******************************************** GETTERS AND SETTERS *********************************************/
+
+    /**
+     * @inheritDoc
+     * @return string string
+     */
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
 
+    /************************************************** ROUTING ***************************************************/
+
+    /*********************************************** PUBLIC METHODS ***********************************************/
+
+    /**
+     * @inheritDoc
+     * @return Crud Crud
+     */
     public function configureFields(string $pageName): iterable
     {
         return array(
