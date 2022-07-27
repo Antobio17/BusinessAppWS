3749f92c03dd91db340af129ad3eec4313c62a3a
Antonio Jiménez <antoniojr997@gmail.com>
2022-07-27 20:38:38 +0200

    :hammer:

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Admin;
 
-use App\Controller\Admin\Interfaces\DashboardControllerInterface;
 use App\Entity\User;
 use RuntimeException;
 use App\Entity\Business;
@@ -10,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use App\Controller\Admin\Interfaces\DashboardControllerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController implements DashboardControllerInterface
