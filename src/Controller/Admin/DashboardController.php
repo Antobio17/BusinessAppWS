f303202dfdbef1fa491d479b3008d43d15c10790
Antonio Jiménez <antoniojr997@gmail.com>
2022-08-20 16:41:00 +0200

    :hammer:

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Shift;
 use App\Entity\User;
 use RuntimeException;
+use App\Entity\Shift;
 use App\Entity\Business;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
