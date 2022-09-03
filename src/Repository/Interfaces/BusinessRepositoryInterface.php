637a3970f6838cc889183e1fa4eb0370e2f41eb7
Antonio Jiménez <antoniojr997@gmail.com>
2022-09-03 22:20:41 +0200

    Nueva propiedad DomainAlias para el contexto de negocio en la aplicación.

--- src/Repository/Interfaces/BusinessRepositoryInterface.php
+++ src/Repository/Interfaces/BusinessRepositoryInterface.php
@@ -18,5 +18,14 @@ interface BusinessRepositoryInterface
      */
     public function findByDomain(string $domain): ?BusinessInterface;
 
+    /**
+     * Finds a Business searching by the domain passed.
+     *
+     * @param string $domain Domain to the searching.
+     *
+     * @return BusinessInterface|null BusinessInterface|null
+     */
+    public function findByAlias(string $domain): ?BusinessInterface;
+
     /*********************************************** STATIC METHODS ***********************************************/
 }
